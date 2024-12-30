f416a2dc86b44d9315bc8ba835c3b74e914d5956
Agema <zamirbekova.aigerim96@gmail.com>
2024-12-30 18:34:38 +0600

    fix/add comments methods

--- src/main/java/payment/PaymentForm.java
+++ src/main/java/payment/PaymentForm.java
@@ -5,7 +5,7 @@ public class PaymentForm {
         int result = sum(5, 10); // Вызов метода sum
         System.out.println("Сумма: " + result); // Вывод результата
     }
-
+    // methods
     public static int sum(int a, int b) {
         return a + b; // Возвращает сумму
     }
